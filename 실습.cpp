00c39691ad82bd87f42bcd382ea0b5021f4f6fcd
20221401 <127109886+20221401@users.noreply.github.com>
2023-03-27 23:30:22 +0900

    Add files via upload

---
+++ 실습.cpp
@@ -0,0 +1,36 @@
+﻿// 실습.cpp
+
+#include<iostream>
+using namespace std;
+
+class CStudent {
+private:
+	string grade;
+
+public:
+	void set_grade(string grade_) {
+		grade = grade_;
+	}
+	string get_grade() {
+		return grade;
+	}
+};
+
+class CProfessor {
+	CStudent OStudent;
+public:
+	void set_student_grade(string grade_) {
+		OStudent.set_grade(grade_);
+	}
+	void print_student_grade() {
+		cout << OStudent.get_grade() << endl;
+	}
+};
+
+int main() {
+	CProfessor OProfessor;
+	OProfessor.set_student_grade("A+");
+	OProfessor.print_student_grade();
+
+	return 0;
+}
